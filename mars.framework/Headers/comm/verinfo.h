809da0806f07c9291260a9033dfe44f591022e0d
wubangmin <wubangmin@yy.com>
2021-03-17 11:49:17 +0800

    add framework

---
+++ mars.framework/Headers/comm/verinfo.h
@@ -0,0 +1,11 @@
+
+#ifndef Mars_verinfo_h
+#define Mars_verinfo_h
+
+#define MARS_REVISION "c3f6dbf5"
+#define MARS_PATH "master"
+#define MARS_URL ""
+#define MARS_BUILD_TIME "2021-03-15 17:02:29"
+#define MARS_TAG ""
+
+#endif
